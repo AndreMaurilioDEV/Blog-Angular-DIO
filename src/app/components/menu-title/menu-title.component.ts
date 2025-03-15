fae8b22981495095199654028d6f2f975b935b68
André Maurilio <andreandrade1920@gmail.com>
2025-03-15 16:58:33 -0300

    blog angular

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-title',
+  standalone: false,
+  templateUrl: './menu-title.component.html',
+  styleUrl: './menu-title.component.css'
+})
+export class MenuTitleComponent implements OnInit{
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+}
